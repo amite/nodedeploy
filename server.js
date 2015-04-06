b53933e8a2bd5bc226d60618ea2c9fe8f5fa3a83
Amit Erandole <amit.erandole@gmail.com>
2015-04-06 23:52:25 +0530

    switched to express

--- server.js
+++ server.js
@@ -1,11 +1,50 @@
-var http = require('http')
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var debug = require('debug');
 
-var fs = require('fs');
-var index = fs.readFileSync('app/index.html');
+// loading routes
+var index = require('./routes/index');
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end(index);
-}).listen(9615, function(){
-  console.log("listening on port 9615")
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// attaching routes to the application
+app.use(index);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+app.set('port', process.env.PORT || 9615);
+
+var server = app.listen(app.get('port'), function() {
+  debug('Express server listening on port ' + server.address().port);
+});
+
+module.exports = app;
