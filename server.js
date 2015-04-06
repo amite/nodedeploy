ccb542566ccdcb45e0bf312c2696356c9211f12b
Amit Erandole <amit.erandole@gmail.com>
2015-04-06 18:36:48 +0530

    basic app setup

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-var http = ''
+var http = require('http')
+
+var fs = require('fs');
+var index = fs.readFileSync('app/index.html');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end(index);
+}).listen(9615, function(){
+  console.log("listening on port 9615")
+});
